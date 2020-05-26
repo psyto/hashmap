3f66541e3226d6047ddc20ef82519eeec495ae5f
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-26 22:09:44 +0900

    implemented till 30 min

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::hash_map::DefaultHasher;
-use std::hash::Hash;
+use std::hash::{Hash, Hasher};
 
 const INITIAL_NBUCKETS: usize = 1;
 
@@ -21,18 +21,23 @@ impl<K, V> HashMap<K, V> {
 
 impl<K, V> HashMap<K, V>
 where
-    K: Hash,
+    K: Hash + Eq,
 {
     pub fn insert(&self, key: K, value: V) -> Option<V> {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        let bucket = hasher.finish() % self.buckets.len();
+        let bucket = (hasher.finish() % self.buckets.len() as u64) as usize;
         let bucket = &mut self.buckets[bucket];
 
-        if let Some(&mut (ref ekey, ref mut evalue)) = bucket.iter_mut().find(|&mut (ref ekey, _)| ekey == key) {
-        } else {
-            bucket.push((key, value));
-        };
+        for &mut (ref ekey, evalue) in bucket {
+            if ekey == key {
+                use std::mem;
+                return Some(mem::replace(evalue, value));
+            }
+        }
+
+        bucket.push((key, value));
+        None
     }
 
     fn resize(&mut self) {
@@ -42,5 +47,16 @@ where
         };
 
         // TODO
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert() {
+        let map = HashMap::new();
+        map.insert("foo", 42);
     }
 }
