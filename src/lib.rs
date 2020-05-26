2e315788dc2dc6e6bb054e8dbcc41b4e72ec17b3
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-26 17:42:16 +0900

    implemented till 10 min

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+struct Bucket<K, V> {
+    items: Vec<(K, V)>,
+}
+
+pub struct HashMap<K, V> {
+    bucket: Vec<Bucket<K, V>>,
 }
